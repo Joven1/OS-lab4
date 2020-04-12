ab87258ff6baa7301726169fd14121a414afb978
Joven Noel Garces <garcesj@shay.ecn.purdue.edu>
2020-04-12 17:12:04 -0400

    Saving

--- save.sh
+++ save.sh
@@ -1,6 +1,12 @@
 #!bin/bash
 
 savefile=$1
-git add *
-git commit -m "Save File: $1"
-git push
+if [[ "$1" == "" ]]
+then
+	echo "Error Empty String"
+else
+	git add *
+	git commit -m "$1"
+	git push
+fi
+
